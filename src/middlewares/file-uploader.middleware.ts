29bb2b41922b6d6dce91fe4cc1be1614c1893c4b
Niyam Rokka Chhetri <chhetriniyam@gmail.com>
2026-01-27 17:56:31 +0545

    Added the missed things

--- src/middlewares/file-uploader.middleware.ts
+++ src/middlewares/file-uploader.middleware.ts
@@ -1,37 +1,23 @@
 import multer from "multer";
 import fs from "fs";
 
-export const upload= ()=> {
 
-    // alternative method
-    // if(!fs.existsSync(folder)){
-    // fs.mkdirSync('uploads/',{recursive:true})
-    // }
-    
-    if (fs.existsSync('uploads')){
+export const upload = () => {
+ let folder ='uploads/'
 
-    const storage = multer.diskStorage({
-        destination:function(req,file,cb)
-        {
-            cb(null,'uploads/')
-        },
-        filename:function(req,file,cb){
-            const fileName = file.fieldname + '-' + Date.now() +'-'+ file.originalname
-            cb(null,fileName)
-        },
-    })
-    return multer({storage})
-}else{
-    fs.mkdirSync('uploads',{recursive:true})
-       const storage = multer.diskStorage({
-        destination:function(req,file,cb)
-        {
-            cb(null,'uploads/')
-        },
-        filename:function(req,file,cb){
-            const fileName = file.fieldname + '-' + Date.now() +'-'+ file.originalname
-            cb(null,fileName)
-        },
-    })
-    return multer({storage})
-}}
+
+  if(!fs.existsSync(folder)){
+    fs.mkdirSync(folder,{recursive:true})
+  }
+
+  const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+      cb(null,folder)
+    },
+    filename:function(req,file,cb){
+      const fileName = file.fieldname + '-'  + Date.now()  + '-' + file.originalname
+      cb(null,fileName)
+    }
+  })
+  return multer({ storage});
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import express from 'express'
-import { login, register } from '../controllers/auth.controller'
+import { login, register, logOut,profile } from '../controllers/auth.controller'
+import { AllUserAndAdmins } from "../types/global.types";
+import { authenticate } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post('/register',register);
 router.post('/login',login);
+router.post("/logout", logOut);
+router.get("/me", authenticate(AllUserAndAdmins), profile);
 
 
 
